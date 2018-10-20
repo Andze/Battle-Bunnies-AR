74e361460925e04c6d8d7934dc10ec7430917867
Andrew Cardwell <andrewcardwell@btinternet.com>
2018-10-20 14:25:43 +0100

    Image tracking done

---
+++ Battle Bunnies/Assets/Scripts/BunnyScript.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BunnyScript : MonoBehaviour {
+
+
+    public void Attack()
+    {
+        gameObject.transform.localScale = new Vector3(2f,2f,2f);
+    }
+}
